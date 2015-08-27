4312530131c300446b4f9cf14b7b51a5bcc0d64f
wangjiezhe <wangjiezhe@gmail.com>
2015-08-27 21:25:52 +0800

    Use timeit and restructure

--- sage/continued_fraction_of_e.py
+++ sage/continued_fraction_of_e.py
@@ -2,9 +2,8 @@
 # coding: utf-8
 
 from __future__ import print_function
-# from functools import partial
-from functools import wraps
-from sage.all import Integer, e, var, symbolic_expression, factorial
+from functools import wraps, partial
+from sage.all import Integer, e, var, symbolic_expression, factorial, timeit
 
 
 def cache(func):
@@ -42,26 +41,58 @@ def series_y(m):
     return series_z(m, Integer(1), Integer(1))
 
 
-# series_x = partial(series_z, m0=Integer(1), m1=Integer(3))
-# series_y = partial(series_z, m0=Integer(1), m1=Integer(1))
-
-
 def series_e(m):
     return series_x(m) / series_y(m)
 
 
-print([series_x(j) for j in range(Integer(10))])
-print([series_y(j) for j in range(Integer(10))])
-print([series_e(j) for j in range(Integer(10))])
-print([series_e(j).n() for j in range(Integer(10))])
-print(e.n())
+def series_z_nocache(m, m0, m1):
+    if m == Integer(0):
+        return m0
+    elif m == Integer(1):
+        return m1
+    else:
+        return (Integer(2) * (Integer(2) * m - Integer(1)) *
+                series_z(m - Integer(1), m0, m1) +
+                series_z(m - Integer(2), m0, m1))
 
 
-t, n = var('t, n')
-__tmp__ = var("t")
-G = symbolic_expression(t**n * (t - Integer(1))**n /
-                        factorial(n) * e**t).function(t)
-__tmp__ = var("n")
-T = symbolic_expression(G.integral(t, Integer(0), Integer(1))).function(n)
-print([T(j) for j in range(Integer(10))])
-print([T(j).n() for j in range(Integer(10))])
+series_x_nocache = partial(series_z_nocache, m0=Integer(1), m1=Integer(3))
+series_y_nocache = partial(series_z_nocache, m0=Integer(1), m1=Integer(1))
+
+
+def series_e_nocache(m):
+    return series_x_nocache(m) / series_y_nocache(m)
+
+
+def main1():
+    print([series_x(j) for j in range(Integer(10))])
+    print([series_y(j) for j in range(Integer(10))])
+    print([series_e(j) for j in range(Integer(10))])
+    print([series_e(j).n() for j in range(Integer(10))])
+    print(e.n())
+
+
+def main2():
+    t, n = var('t, n')
+    G = symbolic_expression(t**n * (t - Integer(1))**n /
+                            factorial(n) * e**t).function(t)
+    T = symbolic_expression(G.integral(t, Integer(0), Integer(1))).function(n)
+    print([T(j) for j in range(Integer(10))])
+    print([T(j).n() for j in range(Integer(10))])
+
+
+def main3():
+    timeit('series_x(Integer(100))')
+    timeit('series_y(Integer(100))')
+    timeit('series_e(Integer(100))')
+    timeit('series_x_nocache(Integer(100))')
+    timeit('series_y_nocache(Integer(100))')
+    timeit('series_e_nocache(Integer(100))')
+
+
+if __name__ == "__main__":
+    main_list = [main for main in dir() if main[:4] == 'main']
+    for main in main_list:
+        print(main + ':')
+        eval(main + '()')
+        print()
